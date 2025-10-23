8b326372c4b4e83d30bb872092c547a2f2a4b2f0
sundaramrathor <rathorsundaram50@gmail.com>
2025-10-23 22:47:17 +0530

    Deploying bug resoved

--- src/app/Blogs/Components/BlogCard.tsx
+++ src/app/Blogs/Components/BlogCard.tsx
@@ -1,7 +1,6 @@
-import Image from 'next/image'
 import React from 'react'
 
-export default function BlogCard({tag, title} : {tag : String, title : string}) {
+export default function BlogCard({tag, title} : {tag : string, title : string}) {
   return (
     <div className='w-36 h-52 rounded-lg outline p-0.5'>
       <img className='h-28 bg-slate-500 rounded-t-lg' src={"https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?semt=ais_hybrid&w=740&q=80"} alt='...' />
