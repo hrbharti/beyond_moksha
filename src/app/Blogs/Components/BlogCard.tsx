57a63897f92d22255deb94c07a91512f6288bb9c
sundaramrathor <rathorsundaram50@gmail.com>
2025-10-23 23:10:36 +0530

    Vercel issue resolved

--- src/app/Blogs/Components/BlogCard.tsx
+++ src/app/Blogs/Components/BlogCard.tsx
@@ -1,9 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function BlogCard({tag, title} : {tag : string, title : string}) {
   return (
     <div className='w-36 h-52 rounded-lg outline p-0.5'>
-      <img className='h-28 bg-slate-500 rounded-t-lg' src={"https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?semt=ais_hybrid&w=740&q=80"} alt='...' />
+      <Image 
+        className='h-28 bg-slate-500 rounded-t-lg' 
+        src={"https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?semt=ais_hybrid&w=740&q=80"} 
+        alt='placeholder' 
+        width={150}
+        height={100}/>
       <p className='text-sm text-white bg-slate-600 w-fit px-4 py-0.5 mt-1'>{tag}</p>
       <h1 className='text-xl mt-1'>{title}</h1>
     </div>
--- src/app/Blogs/Components/SideCard.tsx
+++ src/app/Blogs/Components/SideCard.tsx
@@ -1,10 +1,15 @@
-
+import Image from 'next/image'
 import React from 'react'
 
 export default function SideCard() {
   return (
     <div className='w-full h-28 flex rounded-sm outline-2 outline-slate-600'>
-      <img className='w-2/5 h-full rounded-l-lg' src={"https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?semt=ais_hybrid&w=740&q=80"} alt=''/>
+      <Image 
+      className='w-2/5 h-full rounded-l-lg' 
+      src={"https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?semt=ais_hybrid&w=740&q=80"} 
+      alt='Blog_image'
+      width={100}
+      height={100}/>
       <p className='text-center my-auto'>Understanding funeral attire in India</p>
     </div>
   )
