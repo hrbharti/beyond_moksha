5248916c9d7ac94411538e690d0cf4ee5365d707
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2025-11-06 22:52:04 +0530

    added Suspence and Force-dynamic

--- src/app/tribute/(routes)/memorial/(routes)/page.tsx
+++ src/app/tribute/(routes)/memorial/(routes)/page.tsx
@@ -1,3 +1,7 @@
+"use client"
+
+export const dynamic = "force-dynamic";
+
 import Memorial from "@/app/tribute/ComponentPages/Components/Memorial";
 import Gallery from "@/app/tribute/ComponentPages/Gallery";
 import MemoryWall from "@/app/tribute/ComponentPages/MemoryWall";
@@ -5,17 +9,23 @@ import TimelineSection from "@/app/tribute/ComponentPages/TimelineSection";
 import EventsSection from "@/app/tribute/ComponentPages/EventSection";
 import FamilyTree from "@/app/tribute/ComponentPages/FamilyTree";
 import HeroSection from "@/app/tribute/ComponentPages/Components/MemorialHero";
+import { Suspense } from "react";
 
 export default function Page() {
-  return <div>
-    <HeroSection />
-    <div className="flex-1 md:ml-10 px-5 md:px-10 py-10 transition-all duration-300">
-      <Memorial />
-      <TimelineSection />
-      <Gallery />
-      <MemoryWall />
-      <FamilyTree />
-      <EventsSection />
+  return (
+    <div>
+      <Suspense fallback={<div>Loading hero section...</div>}>
+        <HeroSection />
+      </Suspense>
+
+      <div className="flex-1 md:ml-10 px-5 md:px-10 py-10 transition-all duration-300">
+        <Memorial />
+        <TimelineSection />
+        <Gallery />
+        <MemoryWall />
+        <FamilyTree />
+        <EventsSection />
+      </div>
     </div>
-  </div>;
+  );
 }
