5dada40e422fbb66c590990b354092b61be8dbd0
sundaramrathor <rathorsundaram50@gmail.com>
2025-12-05 12:53:41 +0530

    updated pledge

--- src/app/components/Pledge.tsx
+++ src/app/components/Pledge.tsx
@@ -15,7 +15,7 @@ export const Pledge = () => {
               Our Responsibility
             </h2>
             <p className="text-[#1F3A52] text-sm leading-relaxed">
-              Every life deserves respect. We ensure dignity and care for your loved one in their final journey, and we support those they leave behind.
+              Every life deserves respect. We care for your loved one with the same tenderness and dignity they knew in life, honoring their final journey with compassion. We understand your pain and are here to support you through this difficult time. From our first conversation to the final goodbye and beyond, we walk beside you. Your loved one is safe in our hands, and you are not alone in your grief.
             </p>
           </div>
           </div>
@@ -24,11 +24,11 @@ export const Pledge = () => {
           <div className="md:flex justify-evenly items-end gap-8 mt-10">
             <div className="flex items-stretch">
             <div className="border-2 border-[#1F3A52] p-6 w-full sm:w-[300px] md:w-[320px] shadow-sm">
-              <h2 className="text-[#C89C2E] text-xl mb-3 text-center font-lora font-normal">
+              <h2 className="text-[#C89C2E] text-xl mb-3 text-left font-lora font-normal">
                 Why We Exist
               </h2>
               <p className="text-[#1F3A52] text-sm leading-relaxed">
-                Families need space to mourn, remember, and heal. We organise everything properly so you can focus on saying goodbye and honouring memories.
+                Families need space to mourn, remember, and heal. We take care of everything so you can focus on what matters is being with family and honoring your loved one's memory. During this painful time, you shouldn't carry the burden of arrangements alone. We handle each detail with gentleness and care, guiding you at every step. Let us support you through this journey, so you can grieve, remember, and find comfort together.
               </p>
             </div>
           </div>
@@ -40,8 +40,8 @@ export const Pledge = () => {
                 src={hands}
                 alt="Hands icon"
                 className="object-contain"
-                width={120}
-                height={120}
+                width={220}
+                height={220}
                 priority
               />
             </div>
