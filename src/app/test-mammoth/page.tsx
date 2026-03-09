e3c5a375ddda6d858257817ea5fa6dadea2a4a2c
Tanmay0215 <tanmaygupta.0215@gmail.com>
2026-03-09 23:34:56 +0530

    chore: Remove Mammoth test page and component.

--- src/app/test-mammoth/page.tsx
+++
@@ -1,9 +0,0 @@
-import MammothTestComponent from '@/components/MammothTestComponent';
-
-export default function TestPage() {
-    return (
-        <div className="min-h-screen bg-gray-50">
-            <MammothTestComponent blogId={30} />
-        </div>
-    );
-}
